88130bd0efe5c5a95d20e42aec34f07220211f9c
Hemanth Kumar <96766939+hemz10@users.noreply.github.com>
2025-01-29 08:49:55 +0000

    feat:chatbot

--- Begin/CH_1_NLP_ChatBot.py
+++ Begin/CH_1_NLP_ChatBot.py
@@ -1,19 +1,44 @@
 # Importing TextBlob to help the chatbot understand language nuances.
+from textblob import TextBlob
 
+# Define intents
+intents = {
+    "hours": {
+        "keywords": ["hours", "open", "timing", "time"],
+        "response" : "We are open from 9-6 AM"
+    },
+    "return" : {
+        "keywords": ["close", "refund", "problem"],
+        "response": "Id be happy to help and we are now connecting you to a live agent"
+    }
+}
 
-# Defining the ChatBot class for interaction.
+def get_response(message:str):
+    msg = message.lower()
+    for _, data in intents.items():
+        for keyword in data["keywords"]:
+            if keyword in msg:
+                return data["response"]
+    else:
+        sentiment = TextBlob(message).sentiment.polarity
+        if sentiment > 0:
+            return ("That's so great to hear !!")
+        elif sentiment < 0: 
+            return ("I am so sorry")
+        else:
+            return (" Can you please repeat it !!")
 
-            # Analyzing the sentiment of the user's message.
-            
-
-            # Generating the chatbot's response based on sentiment.
-            
-
-            # Printing the chatbot's response and sentiment.
+def chat():
+    print("Hi !! How can I help you today ?")
+    while (user_msg := input("You: ")):
+        if any(keyword in user_msg.strip().lower() for keyword in ["quit", "bye"]):
+            break
+        print(f"\n ChatBot: {get_response(user_msg)}")
+    
+    print("Thank you for chatting and have a great day !!")
           
 
 
 if __name__ == "__main__":
     # Creating the chatbot and starting the chat loop.
-    chatbot = ChatBot()
-    chatbot.start_chat()
+    chatbot = chat()
